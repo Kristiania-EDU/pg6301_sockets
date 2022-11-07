3b9c81bede3213e59e88e32e5a1914d0dcfa5702
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-11-07 18:49:06 +0100

    Frontend only login

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 
 const element = document.getElementById("app");
 const root = createRoot(element);
 
-root.render(<h1>Hello chat</h1>);
+function Application() {
+  const [username, setUsername] = useState("");
+
+  if (!username) {
+    return <Login onLogin={(username) => setUsername(username)}></Login>;
+  }
+
+  return <div>Hello {username}</div>;
+}
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onLogin(username);
+  }
+
+  return (
+    <div onSubmit={handleSubmit}>
+      <h1>Login</h1>
+
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+root.render(<Application />);
